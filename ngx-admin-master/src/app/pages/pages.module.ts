8e0ca2b28f3de191b4b596e1421540fcb03148b2
Nuttakon <bluehawkpooh9@gmail.com>
2018-06-23 15:29:14 +0700

    aa

---
+++ ngx-admin-master/src/app/pages/pages.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+
+import { PagesComponent } from './pages.component';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { PagesRoutingModule } from './pages-routing.module';
+import { ThemeModule } from '../@theme/theme.module';
+import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
+
+import { AsComponent } from './as/as.component';
+
+const PAGES_COMPONENTS = [
+  PagesComponent,
+];
+
+@NgModule({
+  imports: [
+    PagesRoutingModule,
+    ThemeModule,
+    DashboardModule,
+    MiscellaneousModule,
+  ],
+  declarations: [
+    ...PAGES_COMPONENTS,
+
+    AsComponent,
+  ],
+})
+export class PagesModule {
+}
